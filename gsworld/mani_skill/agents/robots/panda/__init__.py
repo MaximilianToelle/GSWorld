cf160417fc960866b035c1a9cf07367242d5a07b
Maximilian Tölle <maximilian.toelle@dfki.de>
2026-05-05 16:41:20 +0200

    modified the fr3 wrist cam mount to better capture gripper and env and saved as new robot

--- gsworld/mani_skill/agents/robots/panda/__init__.py
+++ gsworld/mani_skill/agents/robots/panda/__init__.py
@@ -1,3 +1,4 @@
 from .fr3_umi import Fr3Umi
 from .fr3_umi_wrist435 import Fr3UmiWrist435
+from .fr3_umi_wrist435_modified import Fr3UmiWrist435Modified
 from .fr3_umi_wrist435_w_mount import Fr3UmiWrist435CamMount
